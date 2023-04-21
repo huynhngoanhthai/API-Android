d41cd561176c027847c22a025964af39db36506b
kai mabo <karickopro@gmail.com>
2023-04-21 10:01:51 +0700

    add featurse forpassword

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -6,7 +6,7 @@ router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.post("/sendmail",authController.sendOTP);
 router.get("/action/:email",authController.setAction);
-router.post("/forgotPassword", authController.forgotPassword);
+router.put("/forgotPassword", authController.forgotPassword);
 
 // router.post("/resetPassword", jwtAuth, authController.resetPassword);
 
